7d9ab3ceee09c29b64e9a287eb3e9fb7ae366c69
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-05 20:46:27 +0300

    [refactoring] fix misstake for main function

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.games.even import even_game
 from brain_games.games.even import description
-from brain_games.main import main
+from brain_games.main import main as main_game
+
+def main():
+    main_game(description, even_game)
 
 if __name__ == '__main__':
     main(description, even_game)
