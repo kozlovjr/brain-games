fe4931197b34c7cb35824309c2500410d79c6504
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-09 18:10:54 +0300

    [FEATURE] add executable file progression play

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,9 @@
+from brain_games.games.progression import progression_game
+from brain_games.games.progression import description
+from brain_games.main import main as main_game
+
+def main():
+    main_game(description, progression_game)
+
+if __name__ == '__main__':
+    main()
