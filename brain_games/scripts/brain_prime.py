34fd2741a0456e71966a09b04be0de117626e06b
nikitakozlovjr <nikita3726898@gmail.com>
2024-04-09 18:10:31 +0300

    [FEATURE] add executable file prime play

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,9 @@
+from brain_games.games.prime import prime_game
+from brain_games.games.prime import description
+from brain_games.main import main as main_game
+
+def main():
+    main_game(description, prime_game)
+
+if __name__ == '__main__':
+    main()
